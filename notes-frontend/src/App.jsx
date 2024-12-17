2fa323cc81b64dc1c55384ea446a6150247321cf
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-17 11:42:45 +0000

    Dec 17, 2024, 5:27 PM

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -55,6 +55,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
