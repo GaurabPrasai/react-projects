8f40831da0306bed0d44a24c263dc30c5b7ca83d
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2025-06-13 21:24:22 +0545

    style: added logo

--- cvgen/src/components/ui/Header.jsx
+++ cvgen/src/components/ui/Header.jsx
@@ -1,15 +1,20 @@
-import { Button } from "./button"
+import { Button } from "./button";
 import { Link } from "react-router-dom";
+import { FileText } from "lucide-react";
 
-const Header = () =>{
-    return(
-        <div className="m-0.5 p-1 justify-between flex items-centre space-x-4">
-            <h1>Logo</h1>
-            <Link to="preview"><Button className="bg-black text-white shadow-md hover:shadow-lg hover:-translate-y-1 transition-all duration-300 ease-out">
-                Preview
-            </Button></Link>            
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className="px-6 py-4 border-b border-border flex items-center justify-between bg-white">
+      <div className="flex items-center gap-2">
+        <img src="src\assets\logo.svg" alt="" srcset="" />
+      </div>
+      <Link to="preview">
+        <Button className="bg-primary text-white font-medium shadow-sm hover:shadow-md hover:bg-primary/90 transition-all duration-200">
+          Preview CV
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
 export default Header;
