3d085d6d05b65235086e5ce93b9876c4d315fb1f
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2025-06-14 09:17:09 +0545

    fix: update logo import path in Header component

--- cvgen/src/components/ui/Header.jsx
+++ cvgen/src/components/ui/Header.jsx
@@ -1,12 +1,13 @@
 import { Button } from "./button";
 import { Link } from "react-router-dom";
+import logo from "/src/assets/logo.svg"
 import { FileText } from "lucide-react";
 
 const Header = () => {
   return (
     <div className="px-6 py-4 border-b border-border flex items-center justify-between bg-white">
       <div className="flex items-center gap-2">
-        <img src="src\assets\logo.svg" alt="" srcset="" />
+        <img src={logo} alt="logo" />
       </div>
       <Link to="preview">
         <Button className="bg-primary text-white font-medium shadow-sm hover:shadow-md hover:bg-primary/90 transition-all duration-200">
